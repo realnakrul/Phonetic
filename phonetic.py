b287aa650ae4a076b4836a1a7f4a29eaf3e3a03b
realnakrul <na_krul@mail.ru>
2018-08-07 14:44:44 +0400

    Letters done, digits and special characters pending

--- phonetic.py
+++ phonetic.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python3
 from sys import argv
-print(argv[1])
-for LETTER in argv[1]:
-    print(LETTER)
+argv.pop(0)
+ALPHABET= {
+'a':'(A)lpha',
+'b':'(B)ravo',
+'c':'(C)harlie',
+'d':'(D)elta',
+'e':'(E)cho',
+'f':'(F)oxtrot',
+'g':'(G)olf',
+'h':'(H)otel',
+'i':'(I)ndia',
+'j':'(J)uliett',
+'k':'(K)ilo',
+'l':'(L)ima',
+'m':'(M)ike',
+'n':'(N)ovember',
+'o':'(O)scar',
+'p':'(P)apa',
+'q':'(Q)uebec',
+'r':'(R)omeo',
+'s':'(S)ierra',
+'t':'(T)ango',
+'u':'(U)niform',
+'v':'(V)ictor',
+'w':'(W)hiskey',
+'x':'(X)-ray',
+'y':'(Y)ankee',
+'z':'(Z)ulu'
+}
+while argv:
+    print('-'*20)
+    print(str(argv[0]))
+    for LETTER in argv[0].lower():
+        print('\t'+ALPHABET[LETTER])
+    argv.pop(0)
+    input('\nPress Enter to continiue...')
